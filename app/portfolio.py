db36ac28dc112f7bcf9acffdb5d60a04604e0d50
Varo <53277209+lVarol@users.noreply.github.com>
2024-10-19 11:23:29 -0500

    Update portfolio.py

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,6 @@
 from flask import Flask, Blueprint, render_template, send_file
 
-
-bp = Blueprint('portfolio',__name__,url_prefix="/")
+bp = Blueprint('portfolio', __name__, url_prefix="/")
 
 @bp.route("/", methods=['GET'])
 def index():
@@ -11,18 +10,15 @@ def index():
 def blog():
     return render_template('portfolio/blogs.html')
 
-@bp.route("/projects",methods=['GET'])
+@bp.route("/projects", methods=['GET'])
 def projects():
     return render_template('portfolio/projects.html')
 
-from flask import send_from_directory
-
-@app.route('/favicon.ico')
+# Example for favicon route
+@bp.route('/favicon.ico', methods=['GET'])
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.png', mimetype='image/png')
-
+    return '', 204  # You can adjust this to return an actual favicon if needed
 
 @bp.errorhandler(404)
-def page_not_found():
-    return render_template('404.html')
+def page_not_found(e):
+    return render_template('404.html'), 404
