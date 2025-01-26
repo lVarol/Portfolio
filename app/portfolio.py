15aa07a7ec963c4458aea17ee8a46c1a21184a52
varo <alvarojose200613@hotmail.com>
2025-01-26 18:16:59 -0500

    ETL-PIPELINE my first blog

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,6 @@
 from flask import Flask, Blueprint, render_template, send_file
 
-
-bp = Blueprint('portfolio',__name__,url_prefix="/")
+bp = Blueprint('portfolio', __name__, url_prefix="/")
 
 @bp.route("/", methods=['GET'])
 def index():
@@ -11,12 +10,21 @@ def index():
 def blog():
     return render_template('portfolio/blogs.html')
 
-@bp.route("/projects",methods=['GET'])
+@bp.route("/projects", methods=['GET'])
 def projects():
     return render_template('portfolio/projects.html')
 
-
+# Example for favicon route
+@bp.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return '', 204  # You can adjust this to return an actual favicon if needed
 
 @bp.errorhandler(404)
-def page_not_found():
-    return render_template('404.html')
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+#Blogs
+
+@bp.route("/ETL-Pipeline", methods=['GET'])
+def etlpipeline():
+    return render_template('portfolio/blogs/ETL-pipeline.html')
