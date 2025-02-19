ca9ad2b86bd4d211b92a77b8ff544ab70fe5dc2a
Álvaro Pineda <53277209+lVarol@users.noreply.github.com>
2025-02-18 23:31:41 -0500

    Update portfolio.py preloads

--- app/portfolio.py
+++ app/portfolio.py
@@ -14,6 +14,17 @@ def blog():
 def projects():
     return render_template('portfolio/projects.html')
 
+#=========================================================================
+@app.route("/log-prerender", methods=["POST"])
+def log_prerender():
+    app.logger.info("Detected a possible prerender/preload!")
+    return ("", 200)
+
+@app.route("/log-foreground", methods=["POST"])
+def log_foreground():
+    app.logger.info("Page is now fully visible (real visit).")
+    return ("", 200)
+#=========================================================================
 # Example for favicon route
 @bp.route('/favicon.ico', methods=['GET'])
 def favicon():
